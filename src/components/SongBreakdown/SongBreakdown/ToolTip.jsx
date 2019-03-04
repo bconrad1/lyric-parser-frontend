bc173184452123ea3ef6c018c2a5e8a1f9f3bb40
Ben <bconrad18@gmail.com>
2019-03-03 19:54:36 -0600

    updating tooltip

--- src/components/SongBreakdown/SongBreakdown/ToolTip.jsx
+++ src/components/SongBreakdown/SongBreakdown/ToolTip.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 const ToolTip = ({xCoord, yCoord, word, wordCount}) => {
   let coordinates = {
     top: `${yCoord}px`,
-    left: `${xCoord}px`
+    left: `${xCoord}px`,
   };
 
-  return(
+  return (
     <div className={'tool-tip-container'}
-        style={coordinates}>
-      <span className={'tool-tip-text'}>{`Word: ${word}`}</span>
-      <span className={'tool-tip-text'}>{`Count: ${wordCount}`}</span>
+         style={coordinates}>
+      <span className={'tool-tip-text'}>Word: <b className={'tool-tip-dynamic'}>{word}</b></span>
+      <span className={'tool-tip-text'}>Count: <b className={'tool-tip-dynamic'}>{wordCount}</b></span>
     </div>
   );
-}
+};
 
 export default ToolTip;
